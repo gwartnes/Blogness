c06bc1aa7bec8974db7a96be6cb40d75b159f2fa
gwartnes <gwartnes@gmail.com>
2015-12-10 13:09:59 -0600

    WritePost view editing

--- src/TestApp3/Models/Admin/WritePostViewModel.cs
+++ src/TestApp3/Models/Admin/WritePostViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,9 @@ namespace TestApp3.Models.Admin
 {
     public class WritePostViewModel
     {
+        [Display(Name = "Post Title")]
         public string Title { get; set; }
+        [Display(Name = "Post Body")]
         public string Body { get; set; }
         public string[] Tags { get; set; }
     }
